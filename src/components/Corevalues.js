6db4b51ac597296269c946d4128517419820ed87
jpnuwagba <jpnuwagaba1@gmail.com>
2022-10-05 19:36:26 +0300

    another commit

--- src/components/Corevalues.js
+++ src/components/Corevalues.js
@@ -5,26 +5,34 @@ const Corevalues = () => {
   return (
     <>
       <div className="container core-values">
-        <Corevalue
-          img='./img/handshake1.png'
-          corevalue='Integrity'
-          corevalueDesc='Delivering the right services the right way through character and honesty'
-        />
-        <Corevalue
-          img='./img/managers.png'
-          corevalue='Professionalism'
-          corevalueDesc='Communicating effectively and appropriately and always finding a way to be productive.'
-        />
-        <Corevalue
-          img='./img/quality.png'
-          corevalue='Quality'
-          corevalueDesc='Using both old unbiased data sources and the new advancing and reliable technology to ensure excellence in service delivery.'
-        />
-        <Corevalue
-          img='./img/landing-icons/mapping (2).png'
-          corevalue='Experience'
-          corevalueDesc='Our industry age of 10 years and still counting guarantees the best workmanship from us.'
-        />
+        <div>
+          <Corevalue
+            img='./img/handshake1.png'
+            corevalue='Integrity'
+            corevalueDesc='Delivering the right services the right way through character and honesty'
+          />
+        </div>
+        <div>
+          <Corevalue
+            img='./img/managers.png'
+            corevalue='Professionalism'
+            corevalueDesc='Communicating effectively and appropriately and always finding a way to be productive.'
+          />
+        </div>
+        <div>
+          <Corevalue
+            img='./img/quality.png'
+            corevalue='Quality'
+            corevalueDesc='Using both old unbiased data sources and the new advancing and reliable technology to ensure excellence in service delivery.'
+          />
+        </div>
+        <div>
+          <Corevalue
+            img='./img/landing-icons/mapping (2).png'
+            corevalue='Experience'
+            corevalueDesc='Our industry age of 10 years and still counting guarantees the best workmanship from us.'
+          />
+        </div>
       </div>
     </>
   )
