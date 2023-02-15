93fa4482f0ad0ff7f571628218c5f842479b3774
jnuwagaba <jpnuwgaba1@gmail.com>
2023-02-15 19:54:40 +0300

    feb23

--- src/components/ClientSection.js
+++ src/components/ClientSection.js
@@ -4,10 +4,11 @@ import GrayButton from './GrayButton'
 const ClientSection = () => {
   return (
     <div className='container client-section'>
-        <GrayButton text={'TRUSTED BY'} />
+        {/* <GrayButton text={'TRUSTED BY'} /> */}
+        <div className='bold'>TRUSTED BY</div>
         <div className="company-logos">
             <img className='company-logo' src="./img/opm.png" alt="" srcSet="" />
-            <img className='company-logo' src="./img/unhcr.png" alt="" srcSet="" />
+            {/* <img className='company-logo' src="./img/unhcr.png" alt="" srcSet="" /> */}
             <img className='company-logo' src="./img/NBRB-logo.png" alt="" srcSet="" />
             <img className='company-logo' src="./img/ndiwa.png" alt="" srcSet="" />
             <img className='company-logo' src="./img/konoike.png" alt="" srcSet="" />
